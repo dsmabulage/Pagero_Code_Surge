d5309973dbdf13f75a8a96629bf881bc6ac97c3f
Dileepa Mabulage <dileepainivos@gmail.com>
2024-11-22 20:04:46 +0530

    update main.py

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import subprocess
 import json
 import os
 
-subprocess.run(["python", "reactscraper.py"], check=True)
-subprocess.run(["scrapy", "crawl", "awslearnspider", "-O", "../aws.json"], check=True)
+os.chdir('reactscraper')
+subprocess.run(['python', 'reactscraper.py'], check=True)
+
+os.chdir('../awsscraper')
+subprocess.run(['scrapy', 'crawl', 'awslearnspider', '-O', '../aws.json'], check=True)
 
 try:
-    os.chdir("..")
+    os.chdir('..')
 
-    with open("react.json", "r", encoding="utf-8") as file1, open(
-        "aws.json", "r", encoding="utf-8"
-    ) as file2:
+    with open('react.json', 'r', encoding='utf-8') as file1, open('aws.json', 'r', encoding='utf-8') as file2:
         data1 = json.load(file1)
         data2 = json.load(file2)
 
     data1.extend(data2)
 
-    with open("combined.json", "w", encoding="utf-8") as outfile:
+    with open('combined.json', 'w', encoding='utf-8') as outfile:
         json.dump(data1, outfile, indent=2, ensure_ascii=False)
 
 except Exception as e:
-    print(f"Error combining JSON files: {e}")
+     print(f"Error combining JSON files: {e}")
+     raise
