a4408c73509f39d6691e6ab85c46186652c152a5
Dileepa Mabulage <dileepainivos@gmail.com>
2024-11-24 09:08:11 +0530

    update with react scraper with sub pages

--- reactscraper/reactscraper.py
+++ reactscraper/reactscraper.py
@@ -1,6 +1,7 @@
 from constant import react_sections, react_base_url
 from utils import get_data_pane, get_soup, json_file_save_path
 import json
+import html2text
 
 
 class ReactDocumentationScraper:
@@ -9,28 +10,18 @@ class ReactDocumentationScraper:
         self.sections = sections
 
     def fetch_links(self):
-        """
-        Fetches links for each specified section and its child links.
-        :return: A list of dictionaries containing parent and child links.
-        """
         links_data = []
         soup = get_soup(f"{self.base_url}/learn")
 
         for section in self.sections:
             try:
                 section_data = self.get_section_links(soup, section)
-                links_data.append(section_data)
+                links_data.extend(section_data)
             except AttributeError:
                 print(f"Warning: Section '{section}' not found on the page.")
         return links_data
 
     def get_section_links(self, soup, section):
-        """
-        Fetches links for a specific section and its child links.
-        :param soup: BeautifulSoup object of the main page
-        :param section: Section title to search for
-        :return: Dictionary with parent link and child links
-        """
         section_parent = soup.find("a", {"title": section}).parent
         section_parent_link = section_parent.find("a")["href"]
 
@@ -42,21 +33,13 @@ class ReactDocumentationScraper:
             for child in section_parent.find_all("li")
         ]
 
-        return {
-            "parent": {
-                "title": section,
-                "link": f"{self.base_url}{section_parent_link}",
-            },
-            "children": child_links,
-        }
+        child_links.insert(
+            0, {"title": section, "link": f"{self.base_url}{section_parent_link}"}
+        )
+
+        return child_links
 
     def fetch_sub_section_content(self, child):
-        """
-        Fetches content for a specific sub-section.
-        :param child: Dictionary containing sub-section information.
-        :return: Dictionary with sub-section title and content.
-        """
-
         child_class = child.findChildren("div", recursive=False)
 
         sub_sections = []
@@ -65,17 +48,21 @@ class ReactDocumentationScraper:
             child_class = child.get("class", [])
             sub_section = {"text": [], "code_snippets": []}
             if "sandpack" in child_class:
-                # Its a code segment
-                sub_section["code_snippets"].append(child.text)
+                # IFrame code block
+                code = child.find("div", class_="sp-code-editor")
+                sub_section["code_snippets"].append(code.text)
+
             else:
                 # Its a nested code block or text
-
                 child_tags = child.findChildren(recursive=False)
 
                 for tag in child_tags:
                     tag_class = tag.get("class", [])
                     if "sandpack" in tag_class:
-                        sub_section["code_snippets"].append(tag.text)
+                        code = tag.find("div", class_="sp-code-editor")
+                        sub_section["code_snippets"].append(
+                            html2text.html2text(code.text)
+                        )
                     else:
                         sub_section["text"].append(tag.text)
 
@@ -83,63 +70,29 @@ class ReactDocumentationScraper:
 
         return sub_sections
 
-    def fetch_section_content(self, links_data):
-        """
-        Fetches content from each section and its child links.
-        :param links_data: List of dictionaries with parent and child link information.
-        :return: A JSON array of scraped data.
-        """
+    def fetch_section_content(self, links):
         json_array = []
 
-        for data in links_data:
+        for data in links:
             try:
+                title, link = data["title"], data["link"]
                 base = {
-                    "title": data["parent"]["title"],
+                    "title": title,
                     "source": "react",
-                    "url": data["parent"]["link"],
+                    "url": link,
                     "sections": [],
                 }
 
-                for child in data["children"]:
-                    try:
-                        child_content = get_data_pane(child["link"])
-                        section_content_list = self.fetch_sub_section_content(
-                            child_content
-                        )
-                        base["sections"].append(
-                            {
-                                "title": child["title"],
-                                "url": child["link"],
-                                "sub_sections": section_content_list,
-                            }
-                        )
-
-                    except Exception as e:
-                        print(
-                            f"Error fetching content for child '{child['title']}' in section '{data['parent']['title']}': {e}"
-                        )
-
-                # Adding Parent section to the JSON array
-                parent_content = get_data_pane(data["parent"]["link"])
-                parent_section_content_list = self.fetch_sub_section_content(
-                    parent_content
-                )
-                # Add as first element
-                base["sections"].insert(
-                    0,
-                    {
-                        "title": data["parent"]["title"],
-                        "url": data["parent"]["link"],
-                        "sub_sections": parent_section_content_list,
-                    },
+                main_content, nav_urls = get_data_pane(link)
+                section_content_list = self.fetch_sub_section_content(main_content)
+                base["sections"].append(
+                    section_content_list,
                 )
 
                 json_array.append(base)
 
             except Exception as e:
-                print(
-                    f"Error fetching content for section '{data['parent']['title']}': {e}"
-                )
+                print(f"Error fetching content for section '{data['title']}': {e}")
 
         return json_array
 
@@ -150,9 +103,9 @@ class ReactDocumentationScraper:
         """
 
         try:
-            links_data = self.fetch_links()
+            urls = self.fetch_links()
 
-            json_array = self.fetch_section_content(links_data)
+            json_array = self.fetch_section_content(urls)
 
             with open(json_file_save_path(), "w", encoding="utf-8") as json_file:
                 json.dump(json_array, json_file, indent=2, ensure_ascii=False)
