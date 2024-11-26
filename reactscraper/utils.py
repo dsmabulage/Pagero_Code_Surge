5d9ca45c28cb75463c90c2cc0d6ad5bc8db7b5e8
Dileepa Mabulage <dileepainivos@gmail.com>
2024-11-26 21:32:53 +0530

    remove white spaces

--- reactscraper/utils.py
+++ reactscraper/utils.py
@@ -2,16 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
-
 def json_file_save_path():
     path = os.getcwd() 
     return os.path.join(os.path.abspath(os.path.join(path, os.pardir)), "react.json")
 
-
 def get_soup(url):
     response = requests.get(url)
     return BeautifulSoup(response.content, "html.parser")
-
 
 def get_data_pane(url):
     soup = get_soup(url)
